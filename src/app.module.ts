3edf93fb6b3e5eea13ffdf3e0f5c0b525d651b5c
Gabriel Dias <gabrieldiassantiago@hotmail.com>
2025-02-17 13:55:50 -0300

    feat: novas att

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { WhatsappModule } from './zap/zap.module';
 import { NeighborhoodModule } from './bairros/bairros.module';
 import { CategoryModule } from './category/category.module';
 import { AdditionModule } from './addition/additio.module';
+import { HealthCheckService } from './health-check.service';
+import { AppController } from './app.controller'; // Adicionar importação do controlador
+import { AppService } from './app.service'; // Adicionar importação do serviço
 
 @Module({
-  imports: [PrismaModule, ProductModule, OrderModule, WhatsappModule, NeighborhoodModule, CategoryModule, AdditionModule ],
-  providers: [],
-
+  imports: [PrismaModule, ProductModule, OrderModule, WhatsappModule, NeighborhoodModule, CategoryModule, AdditionModule],
+  controllers: [AppController], // Registrar controlador
+  providers: [HealthCheckService, AppService], // Registrar serviço
 })
 export class AppModule {}
---
+++ src/health-check.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as cron from 'node-cron';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+
+@Injectable()
+export class HealthCheckService {
+    private readonly logger = new Logger(HealthCheckService.name);
+
+    constructor() {
+        this.scheduleHealthCheck();
+    }
+
+    private scheduleHealthCheck() {
+        cron.schedule('*/14 * * * *', () => {
+            this.logger.log('Checking API health');
+            this.checkHealth();
+        });
+    }
+
+    private async checkHealth() {
+        const apiUrl = 'http://localhost:3000/health';
+        try {
+            const response = await fetch(apiUrl);
+            if (response.status === 200) {
+                this.logger.log('API is healthy');
+            } else {
+                this.logger.error('API is unhealthy');
+            }
+        } catch (error) {
+            this.logger.error('Error checking API health', error);
+        }
+    }
+}
